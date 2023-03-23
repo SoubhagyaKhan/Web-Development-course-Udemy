e139b767a283f48433d04533011d395045d56c57
SoubhagyaKhan <khan.soubhagya2003@gmail.com>
2023-03-23 16:05:04 +0530

    practice

---
+++ datatypes.js
@@ -0,0 +1,17 @@
+let name="Soubhagya"
+console.log(name)
+
+const num=98;
+console.log(num);
+
+const record={
+      John :31,
+      Garima: 21,
+      Raj: 35
+}
+
+console.log(record);
+console.table(record);
+
+var arr=[100,50,60]
+console.log(arr+" is of type "+typeof(arr))
